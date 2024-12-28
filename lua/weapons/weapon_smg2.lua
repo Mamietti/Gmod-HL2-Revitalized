84753cf5932b54f8b8b61641848dcc76b78581a8
mamietti <mauri.miettinen13@gmail.com>
2024-12-29 00:12:43 +0200

    Remove unnecessary hold type setup

--- lua/weapons/weapon_smg2.lua
+++ lua/weapons/weapon_smg2.lua
@@ -102,19 +102,3 @@ function SWEP:FireNPCPrimaryAttack( pOperator, vecShootOrigin, vecShootDir )
 	pOperator:MuzzleFlash();
 	self:SetClip1(self:Clip1() - 1)
 end
-
-function SWEP:SetupWeaponHoldTypeForAI( t )
-
-	self.ActivityTranslateAI = {}
-	self.ActivityTranslateAI [ ACT_STAND ] 						= ACT_STAND
-	self.ActivityTranslateAI [ ACT_IDLE_ANGRY ] 				= ACT_IDLE_ANGRY_SMG1
-
-	self.ActivityTranslateAI [ ACT_MP_RUN ] 					= ACT_HL2MP_RUN_SMG1
-	self.ActivityTranslateAI [ ACT_MP_CROUCHWALK ] 				= ACT_HL2MP_WALK_CROUCH_SMG1
-
-	self.ActivityTranslateAI [ ACT_RANGE_ATTACK1 ] 				= ACT_RANGE_ATTACK_SMG1
-	self.ActivityTranslateAI [ ACT_RANGE_ATTACK1_LOW ] 				= ACT_RANGE_ATTACK_SMG1_LOW
-
-	self.ActivityTranslateAI [ ACT_RELOAD ] 					= ACT_RELOAD_SMG1
-
-end
